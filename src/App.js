d12d1d444e4beb6dff4499eaf8582fa2bd860ad5
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-14 09:57:37 -0400

    seperating funcs from App

--- src/App.js
+++ src/App.js
@@ -5,35 +5,61 @@ import Card from "./components/Card";
 
 function App() {
   const [bgc, setBgc] = React.useState("#000");
+  const [quotes, setQuotes] = React.useState([
+    {
+      text: "Say Hello to My Little Friend!",
+      author: "Tony Montana",
+    },
+  ]);
+  const [randomNum, setRandomNum] = React.useState(0);
+
+  const generateRandomNum = () => {
+    const randomNum = Math.floor(Math.random() * 100);
+    if (randomNum >= 16) generateRandomNum();
+    else {
+      // setRandomNum(randomNum);
+      return randomNum;
+    }
+  };
+
+  const randomColorGenerator = () =>
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+  // setBgc("#" + Math.floor(Math.random() * 16777215).toString(16));
+
+  const fetchQuote = async () => {
+    const res = await fetch("https://type.fit/api/quotes");
+    return await res.json();
+    // setQuotes(data);
+  };
 
   React.useEffect(() => {
-    const fetchColor = async () => {
-      const res = await fetch("https://www.colr.org/json/color/random");
-      const { new_color } = await res.json();
-      console.log(new_color);
-      setBgc("#" + new_color);
-      console.log(bgc);
-    };
-
-    fetchColor();
+    generateRandomNum();
+    fetchQuote();
   }, []);
+
+  React.useEffect(() => {
+    randomColorGenerator();
+  }, [randomNum]);
 
   return (
     <div className="App" style={{ backgroundColor: bgc }}>
       <Card>
-        <h1 id="text">Say Hello to My Little Friend!</h1>
-        <p id="author">Tony Montana</p>
+        <h1 id="text">{quotes[randomNum].text}</h1>
+        <p id="author">{quotes[randomNum].author}</p>
         <div>
           <div>
-            <button id="new-quote">New Quote</button>
+            <button onClick={generateRandomNum} id="new-quote">
+              New Quote
+            </button>
           </div>
           <div>
             <a
               href="https://www.twitter.com/intent/tweet"
+              rel="noreferrer"
               id="tweet-quote"
               target="_blank"
             >
-              x
+              twitter
             </a>
           </div>
         </div>
